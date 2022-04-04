9c951e6801dae7a848d37c2a4801cde64fdcd480
Rich Bayliss <richbayliss@gmail.com>
2022-04-04 11:12:12 +0100

    Tweak systemd

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,10 +8,10 @@ zerotier-cli join $ZT_NETWORK
 zerotier-cli set $ZT_NETWORK allowManaged=0
 
 while true; do
-  brctl show box0 | grep ztbpaltgeu > /dev/null 2>&1
-  if [ "$?" != "0" ]; then
-    brctl addif box0 ztbpaltgeu
-    printf "### ZeroTier interface added to bridge"
-  fi
+  # brctl show box0 | grep ztbpaltgeu > /dev/null 2>&1
+  # if [ "$?" != "0" ]; then
+  #   brctl addif box0 ztbpaltgeu
+  #   printf "### ZeroTier interface added to bridge"
+  # fi
   sleep 5
 done
