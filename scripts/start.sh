ed09ce6b1c5c721228dd79837ab3ed84a4387453
Flynn Joffray <nucleardreamer@gmail.com>
2022-04-04 12:08:10 -0700

    fixing up start script
    Signed-off-by: Flynn Joffray <nucleardreamer@gmail.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,12 +8,18 @@ zerotier-cli join $ZT_NETWORK
 # zerotier-cli set $ZT_NETWORK allowManaged=0
 
 PHY_IFACE="$(ip route | grep default | cut -d' ' -f5)"
-ZT_IFACE="$(zerotier-cli listnetworks | tail -n1 | cut -d' ' -f9)"
+
+# cut -f9 returns IP block (or - if no IP address), -f8 will return the network interface
+ZT_IFACE="$(zerotier-cli listnetworks | tail -n1 | cut -d' ' -f8)"
 
 sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -o $PHY_IFACE -j MASQUERADE
-iptables -A FORWARD -i $ZT_IFACE -o $PHY_IFACE -j ACCEPT
-iptables -A FORWARD -i $PHY_IFACE -o $ZT_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# looping for multiple physical interfaces
+for IFACE in ${PHY_IFACE//\s/ }; do
+  iptables -t nat -A POSTROUTING -o $IFACE -j MASQUERADE
+  iptables -A FORWARD -i $ZT_IFACE -o $IFACE -j ACCEPT
+  iptables -A FORWARD -i $IFACE -o $ZT_IFACE -m state --state RELATED,ESTABLISHED -j ACCEPT
+done
 
 while true; do
   # brctl show box0 | grep ztbpaltgeu > /dev/null 2>&1
